2e190b6c42112d0d3c2cef82c30747ee63cad709
Felicia <ffeliciachang@gmail.com>
2024-12-31 11:42:23 -0800

    env var

--- app/db.ts
+++ app/db.ts
@@ -3,8 +3,7 @@ import { init } from "@instantdb/react";
 let INSTANT_APP_ID = process.env.NEXT_PUBLIC_INSTANT_APP_ID;
 
 if (!INSTANT_APP_ID) {
-  console.error("Please set the INSTANT_APP_ID environment variable");
-  INSTANT_APP_ID = "";
+  throw Error("Please set the NEXT_PUBLIC_INSTANT_APP_ID environment variable");
 }
 
 export const db = init({ appId: INSTANT_APP_ID });
