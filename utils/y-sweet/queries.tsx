f9491c583091bedb04176856874ac470504c6122
Felicia <ffeliciachang@gmail.com>
2024-12-31 11:45:49 -0800

    auth notice

--- utils/y-sweet/queries.tsx
+++ utils/y-sweet/queries.tsx
@@ -2,6 +2,8 @@
 import { manager } from "./y-sweet-document-manager";
 
 export async function createDoc() {
+  // In a production app, this is where you'd authenticate the user
+  // and check that they are authorized to access the doc.
   const ysweetDoc = await manager.createDoc();
 
   return {
